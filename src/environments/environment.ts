b05df67550cb3d96be9336abf5290a01174d7c46
NELSON Pereira de Castro <nelsoncastro.eireli@poupex.com.br>
2022-03-31 14:40:40 -0300

    First commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    PARSE_APP_ID: 'DNXqGWmTLFXbZjAhVhNEtr6T7dl17V5nZtvEgUY5',
+    PARSE_JS_KEY: 'iTGXQgPmDJf2cWrWq8YBR73IedUBG9eRT9lVY8rd',
+    serverURL: 'https://parseapi.back4app.com'
+  };
